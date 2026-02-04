83b4b7f7520dbd885f381073084ac445639dbd54
Manavk1397 <manavkhatri1111@gmail.com>
2026-02-04 11:26:55 +0530

    all routing work

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) 
+  },
+  { 
+    path: 'admin', 
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) 
+  },
+  { 
+    path: 'customer', 
+    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) 
+  },
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth/login' } // Handle 404s by redirecting to login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/customer/customer-routing.module.ts
+++ frontend/src/app/customer/customer-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './history/history.component';
+import { RoomlistComponent } from './roomlist/roomlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'rooms', component: RoomlistComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: '', redirectTo: 'rooms', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
