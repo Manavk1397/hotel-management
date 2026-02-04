99e9f75864125a028f3b240f66b3b18c5bd728d5
Manavk1397 <manavkhatri1111@gmail.com>
2026-02-04 11:19:47 +0530

    generated components for customer

--- frontend/src/app/customer/customer.module.ts
+++ frontend/src/app/customer/customer.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
+import { RoomlistComponent } from './roomlist/roomlist.component';
+import { HistoryComponent } from './history/history.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RoomlistComponent,
+    HistoryComponent
+  ],
   imports: [
     CommonModule,
     CustomerRoutingModule
