a98fcbe071476b0cde2fb7616b225a37e37fbe5e
Manavk1397 <manavkhatri1111@gmail.com>
2026-02-04 11:18:52 +0530

    generated components for admin

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManageroomsComponent } from './managerooms/managerooms.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    ManageroomsComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
---
+++ frontend/src/app/admin/managerooms/managerooms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managerooms',
+  templateUrl: './managerooms.component.html',
+  styleUrls: ['./managerooms.component.css']
+})
+export class ManageroomsComponent {
+
+}
