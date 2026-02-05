266724fb26e1b34bd1efe7fc2f2b6409773c628f
Manavk1397 <manavkhatri1111@gmail.com>
2026-02-05 14:25:09 +0530

    basic pages working

--- frontend/src/app/customer/roomlist/roomlist.component.ts
+++ frontend/src/app/customer/roomlist/roomlist.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./roomlist.component.css']
 })
 export class RoomlistComponent {
+// Using basic variables and arrays
+  room1Type = 'Deluxe Room';
+  room1Price = 2500;
+  room1Count = 5;
 
+  room2Type = 'Executive Suite';
+  room2Price = 5000;
+  room2Count = 2;
+
+  bookRoom1() {
+    this.room1Count = this.room1Count - 1;
+    alert('Booking Successful for Deluxe!');
+}
 }
