79bd2756f2cc5ae46a1ada7905eb44f8cf3302fb
Manavk1397 <manavkhatri1111@gmail.com>
2026-02-10 13:17:38 +0530

    room model

--- frontend/src/app/shared/roomcard/roomcard.component.ts
+++ frontend/src/app/shared/roomcard/roomcard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-roomcard',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./roomcard.component.css']
 })
 export class RoomcardComponent {
+  // Fixes the "Property does not exist" error
+  @Input() buttonLabel: string = 'Book Now'; 
 
+  // Allows the parent to pass room details
+  @Input() roomData: any; 
+
+  // Allows the card to send data back to the parent when clicked
+  @Output() cardAction = new EventEmitter<any>();
+
+  onBtnClick() {
+    this.cardAction.emit(this.roomData);
+  }
 }
